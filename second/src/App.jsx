7c65a5fd41ff8a7686d67c6d2899f3af99c3d927
azareaan <azareaan@gmail.com>
2025-03-06 14:17:32 +0330

    build gallery components to second project

--- second/src/App.jsx
+++ second/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Gallery from './assets/components/Gallery'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [image, setImage] = useState([])
+
+
+  useEffect(() => {
+      fetch(`https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}&query=nature&orientation=landscape`)
+      .then(response => response.json())
+      .then(data => setImage(data))
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <Gallery images={image} /> */}
     </>
   )
 }
---
+++ second/src/assets/components/Gallery.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Gallery.css';
+
+
+const Gallery = (images) => {
+    return (
+        <div className="gallery">
+            {images?.map((image, index) => (
+                <div key={index} className="gallery-item">
+                    <img src={image} alt={`Gallery ${index + 1}`} />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Gallery;
