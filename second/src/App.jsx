55d3d75c753f15d22572dbf6c3f240ca2930dead
azareaan <azareaan@gmail.com>
2025-03-07 00:21:52 +0330

    use axios in second project

--- second/src/App.jsx
+++ second/src/App.jsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Gallery from './assets/components/Gallery'
+import axios from "axios"
 
 function App() {
-  const [image, setImage] = useState([]);
+  const [image, setImage] = useState([
+    
+  ]);
   const [counter, setCounter] = useState(0);
 
   const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
   const url = `https://api.unsplash.com/photos/random?count=4&client_id=${accessKey}`
 
   useEffect(() => {
-    fetch(url)
-    .then(response => response.json())
-    .then(data => setImage(prevImages => [...prevImages, ...data]))
-    .catch(error => console.error("Error fetching images:", error));
+    axios.get(url)
+    .then((res) => setImage(prev => [...prev, ...res.data]))
+    .catch((error) => console.error("Error fetching images:", error));
   }, [counter])  
 
   return (
