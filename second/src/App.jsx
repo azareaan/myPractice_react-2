7022aec8fad131c56ee353d6493c06d96298c2b3
azareaan <azareaan@gmail.com>
2025-03-06 17:24:43 +0330

    fix gallery bug in second project

--- second/src/App.jsx
+++ second/src/App.jsx
@@ -3,18 +3,34 @@ import './App.css'
 import Gallery from './assets/components/Gallery'
 
 function App() {
-  const [image, setImage] = useState([])
+  const [image, setImage] = useState([
+      "./src/assets/image/image1.jpg",
+      "./src/assets/image/image2.jpg",
+      "./src/assets/image/image3.jpg",
+      "./src/assets/image/image4.jpg",
+      "./src/assets/image/image5.jpg",
+      "./src/assets/image/image6.jpg",
+      "./src/assets/image/image7.jpg",
+      "./src/assets/image/image8.jpg",
+      "./src/assets/image/image9.jpg",
+      "./src/assets/image/image10.jpg",
+      "./src/assets/image/image11.jpg",
+      "./src/assets/image/image12.jpg"
+    ]);  
 
+  const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
 
   useEffect(() => {
-      fetch(`https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}&query=nature&orientation=landscape`)
-      .then(response => response.json())
-      .then(data => setImage(data))
+    fetch(`https://api.unsplash.com/photos/random?count=5&client_id=${accessKey}`)
+    .then(response => response.json())
+    .then(data => { setImage(data)})
+    .catch(error => console.error("Error fetching images:", error));
   }, [])
 
   return (
     <>
-      {/* <Gallery images={image} /> */}
+    <h1>gallery</h1>
+      <Gallery images={image}/>
     </>
   )
 }
--- second/src/assets/components/Gallery.jsx
+++ second/src/assets/components/Gallery.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useEffect, useState } from 'react'
 import './Gallery.css';
 
 
-const Gallery = (images) => {
+const Gallery = ({images}) => {
+        
     return (
         <div className="gallery">
+
             {images?.map((image, index) => (
                 <div key={index} className="gallery-item">
                     <img src={image} alt={`Gallery ${index + 1}`} />
                 </div>
             ))}
+
         </div>
     );
 };
